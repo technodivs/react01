db6697d05683363d5f31b5180f4652f8b11603c3
Devanshi <devanshisri0805@gmail.com>
2025-11-12 14:10:12 +0530

    new folder added

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(0)
+  const addValue = () =>{
+   
+    console.log("Add Value Button Clicked", counter);
+    counter = counter + 1
+    setCounter(counter)
+  }
+//  let counter = 5
+  return (
+    <>
+        <h1>Chai aur React</h1>
+        <h2>Counter Value:{counter}</h2>
+        <button onClick={addValue}>Add Value</button>
+        <br />
+        <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
